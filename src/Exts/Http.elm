a5f07b939e54f9a527a89598bece8084947bf5b0
Kris Jenkins <krisajenkins@gmail.com>
2015-03-24 22:54:16 +0000

    Adding Http.map.

--- src/Exts/Http.elm
+++ src/Exts/Http.elm
@@ -8,6 +8,13 @@ import Set (Set)
 import Http
 import Http (..)
 
+map : (a -> b) -> Response a -> Response b
+map f r =
+  case r of
+    Success v -> Success (f v)
+    Waiting -> Waiting
+    Failure n e -> Failure n e
+
 isWaiting : Response a -> Bool
 isWaiting response =
   case response of
