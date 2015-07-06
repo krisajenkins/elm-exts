8d5b4e91cc638c159b1e46983a891c5647fe16f7
Kris Jenkins <krisajenkins@gmail.com>
2015-07-06 13:54:36 +0100

    Making Exts.Set.uniqueItems more generic.

--- src/Exts/Set.elm
+++ src/Exts/Set.elm
@@ -8,5 +8,5 @@ import Set as Set exposing (Set, member)
 select : (a -> comparable) -> Set comparable -> List a -> List a
 select f keys = filter (\x -> member (f x) keys)
 
-uniqueItems : (a -> Maybe String) -> List a -> Set String
+uniqueItems : (a -> Maybe comparable) -> List a -> Set comparable
 uniqueItems accessor data = Set.fromList (List.filterMap accessor data)
