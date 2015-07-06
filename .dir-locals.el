55b6633e3502af436b7e9dceb43dbc59fcc4a0ab
Kris Jenkins <krisajenkins@gmail.com>
2015-07-06 08:47:52 +0100

    Updating the Emacs config for Elm 0.15.1 and Magit 2.1.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,11 +2,11 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil (eval local-set-key
-               (kbd "M-e")
-               '(lambda ()
-                  (interactive)
-                  (require 'magit)
-                  (let ((compile-command (format "cd %s ; elm make"
-                                                 (magit-get-top-dir))))
-                    (save-buffer)
-                    (compile compile-command))))))
+            (kbd "M-e")
+            '(lambda ()
+               (interactive)
+               (require 'magit)
+               (let ((compile-command (format "cd %s.. ; elm-make --warn"
+                                              (magit-git-dir))))
+                 (save-buffer)
+                 (compile compile-command))))))
