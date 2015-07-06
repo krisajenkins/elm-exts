4cc32a532054da053fa71e9a93529f67b601f277
Kris Jenkins <krisajenkins@gmail.com>
2015-07-06 08:57:25 +0100

    Removing SafeList. 0.15 made it obsolete.

--- src/Exts/SafeList.elm
+++
@@ -1,9 +0,0 @@
-module Exts.SafeList (
-  lookup
-  ) where
-
-{-| A safe version of the List library, following Haskell's Safe.
--}
-
-lookup : (a -> Bool) -> List a -> Maybe a
-lookup f xs = List.filter f xs |> List.head
