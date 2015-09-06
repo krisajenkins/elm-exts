bd8bec9181c8f250aaecbb9f97d363bdaa182ad0
Kris Jenkins <krisajenkins@gmail.com>
2015-09-06 21:55:05 +0100

    Adding an onEnter event handler.

---
+++ src/Exts/Html/Events.elm
@@ -0,0 +1,29 @@
+module Exts.Html.Events
+  (onEnter)
+  where
+
+{-| Extensions to the Html.Events library.
+
+@docs onEnter
+-}
+
+import Html exposing (Attribute)
+import Html.Events exposing (on, keyCode)
+import Json.Decode exposing (customDecoder)
+import Signal exposing (Message)
+
+keyCodeIs : Int -> Int -> Result String ()
+keyCodeIs expected actual =
+  if expected == actual
+  then Ok ()
+  else Err "Not the right key code"
+
+enterKey : Int -> Result String ()
+enterKey = keyCodeIs 13
+
+{-| Send a message when the user hits enter. -}
+onEnter : Message -> Attribute
+onEnter message =
+    on "keydown"
+      (customDecoder keyCode enterKey)
+      (always message)
