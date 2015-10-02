5edee36b098c49804a0ad2c402b09a7206b9e54f
Kris Jenkins <krisajenkins@gmail.com>
2015-10-02 17:18:04 +0100

    Improving a docstring.

--- src/Exts/List.elm
+++ src/Exts/List.elm
@@ -12,7 +12,7 @@ import Dict
 
   Be aware that the last sub-list may be smaller than n-items long.
 
-  For example `chunk 3 [1,2,3,4,5,6,7,8,9,10] => [[1,2,3], [4,5,6], [7,8,9], [10]]`
+  For example `chunk 3 [1..10] => [[1,2,3], [4,5,6], [7,8,9], [10]]`
 -}
 chunk : Int -> List a -> List (List a)
 chunk n xs =
