7241b304a492c7dc66e5d8aff23b08c756c67660
Kris Jenkins <krisajenkins@gmail.com>
2015-10-20 17:19:38 +0100

    Adding some Http utilities.

---
+++ src/Exts/Http.elm
@@ -0,0 +1,38 @@
+module Exts.Http
+       (put, postForm)
+       where
+
+{-| Extensions to the main Http library.
+
+@docs put, postForm
+-}
+
+import Http exposing (..)
+import Json.Decode exposing (Decoder)
+import Task exposing (Task, andThen, mapError, succeed, fail)
+
+promoteError : RawError -> Error
+promoteError rawError =
+  case rawError of
+    RawTimeout -> Timeout
+    RawNetworkError -> NetworkError
+
+{-| Send a simple PUT request. -}
+put : Decoder value -> String -> Http.Body -> Task Error value
+put decoder url body =
+  let request =
+        { verb = "PUT"
+        , headers = []
+        , url = url
+        , body = body}
+  in fromJson decoder (send Http.defaultSettings request)
+
+{-| Send a POST request with appropriate headers form-encoding. -}
+postForm : Decoder value -> String -> Body -> Task Error value
+postForm decoder url body =
+  let request =
+        { verb = "POST"
+        , headers = [("Content-Type", "application/x-www-form-urlencoded")]
+        , url = url
+        , body = body}
+  in fromJson decoder (Http.send Http.defaultSettings request)
