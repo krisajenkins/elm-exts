c05ddacda501336b89bf0be59c7f60d8220042f3
Kris Jenkins <krisajenkins@gmail.com>
2015-11-18 09:15:03 +0000

    Switching to the NoRedInk port of elm-check.

--- test/Tests.elm
+++ test/Tests.elm
@@ -5,9 +5,9 @@ import ElmTest.Runner.Element exposing (runDisplay)
 import Tests.Exts.Date
 import Tests.Exts.Delta
 import Tests.Exts.List
-import Html exposing (Html,div)
-import Check exposing (Claim, quickCheck)
-import Check.Runner.Browser exposing (display)
+import Html exposing (..)
+import Html.Attributes exposing (style)
+import Check exposing (Claim,quickCheck,Evidence(..),FailureOptions,SuccessOptions)
 
 tests : Test
 tests =
@@ -22,5 +22,46 @@ claims =
 
 main : Html
 main = div []
-           [runDisplay tests |> Html.fromElement
-           ,quickCheck claims |> display]
+           [h2 [] [text "Unit Tests"]
+           ,runDisplay tests |> Html.fromElement
+           ,h2 [] [text "Property Tests"]
+           ,quickCheck claims |> displayEvidence]
+
+------------------------------------------------------------
+
+displayFailure : FailureOptions -> Html
+displayFailure failure =
+  table [style [("color", "red")]]
+        [thead []
+               [tr []
+                   [th [] [text "Name"]
+                   ,th [] [text "Expected"]
+                   ,th [] [text "Actual"]
+                   ,th [] [text "Counterexample"]]]
+        ,tbody []
+               [tr []
+                   [td [] [text failure.name]
+                   ,td [] [text failure.expected]
+                   ,td [] [text failure.actual]
+                   ,td [] [text failure.counterExample]]]]
+
+displaySuccess : SuccessOptions -> Html
+displaySuccess success =
+  table [style [("color", "green")]]
+        [thead []
+               [tr []
+                   [th [] [text "Name"]
+                   ,th [] [text "Checks"]]]
+        ,tbody []
+               [tr []
+                   [td [] [text success.name]
+                   ,td [] [text (toString success.numberOfChecks)]]]]
+
+
+displayEvidence : Evidence -> Html
+displayEvidence evidence =
+  case evidence of
+    Multiple title children -> div [style [("margin-left", "20px")]]
+                                   (h4 [] [text title]::(List.map displayEvidence children))
+    Unit (Err error) -> displayFailure error
+    Unit (Ok result) -> displaySuccess result
