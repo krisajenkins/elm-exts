717f8c7693174516efad692de4b8d7a8eb57b9ac
Kris Jenkins <krisajenkins@gmail.com>
2016-02-16 15:45:15 +0000

    Adding a simple validation library.

---
+++ src/Exts/Validation.elm
@@ -0,0 +1,73 @@
+module Exts.Validation (..) where
+
+{-| Simple tools for validation. See also [Richard Feldman's elm-validate](http://package.elm-lang.org/packages/rtfeldman/elm-validate/latest)
+
+@docs apply, (|:), required, notBlank, emailRegex
+-}
+
+import Regex exposing (Regex, caseInsensitive, regex, contains)
+import Result exposing (andThen, map)
+
+
+{-| A validator is a function that takes a possibly-invalid form, and
+either returns an error message, or a form that is definitely valid. For example:
+
+    type alias Form =
+      { email : Maybe String
+      , age : Maybe Int
+      }
+
+    type alias ValidForm =
+      { email : String
+      , age : Int
+      }
+
+    validateForm : Form -> Result String ValidForm
+    validateForm form =
+      Ok ValidForm
+        |: (notBlank "Email is required and may not be blank." form.email)
+        |: (required "Age is required" form.age)
+
+(Hat tip to CircuitHub, who inspired the syntax and guided the code with their [elm-json-extra library](http://package.elm-lang.org/packages/circuithub/elm-json-extra/latest))
+-}
+apply : Result e (a -> b) -> Result e a -> Result e b
+apply f aResult =
+  f `andThen` (\f' -> f' `map` aResult)
+
+
+{-| Convenient synonym for `apply`.
+-}
+(|:) : Result e (a -> b) -> Result e a -> Result e b
+(|:) =
+  apply
+
+
+{-| A field that might be Nothing, but is only valid if it is (Just x).
+-}
+required : e -> Maybe a -> Result e a
+required err =
+  Maybe.withDefault (Err err) << Maybe.map Ok
+
+
+{-| A field that might be Nothing, but is only valid if it is a non-empty string.
+-}
+notBlank : e -> Maybe String -> Result e String
+notBlank err str =
+  case str of
+    Nothing ->
+      Err err
+
+    Just "" ->
+      Err err
+
+    Just x ->
+      Ok x
+
+
+{-| A basic email regex. This is incredibly simplistic, but is
+included for convenience. Remember that the only real way to validate
+an email address is to send something to it and get a reply.
+-}
+emailRegex : Regex
+emailRegex =
+  caseInsensitive (regex ".+@.+\\..+")
