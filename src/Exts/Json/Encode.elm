43ee1cd8d87199b49b01064385426518b267e410
Kris Jenkins <krisajenkins@gmail.com>
2016-02-22 16:12:51 +0000

    Adding JSON encoders for 2-tuples and dicts.

--- src/Exts/Json/Encode.elm
+++ src/Exts/Json/Encode.elm
@@ -2,11 +2,12 @@ module Exts.Json.Encode (..) where
 
 {-| Extensions to the core Json Encode library.
 
-@docs set
+@docs set, dict, tuple2
 -}
 
 import Json.Encode exposing (..)
 import Set as Set exposing (Set)
+import Dict exposing (Dict)
 
 
 {-| Encode a set to a JSON array .
@@ -14,3 +15,17 @@ import Set as Set exposing (Set)
 set : (comparable -> Value) -> Set comparable -> Value
 set encodeElement =
   list << List.map encodeElement << List.sort << Set.toList
+
+
+{-| Encode a 2-tuple to a JSON array .
+-}
+tuple2 : (a -> Value) -> (b -> Value) -> ( a, b ) -> Value
+tuple2 encodeKey encodeValue ( x, y ) =
+  list [ encodeKey x, encodeValue y ]
+
+
+{-| Encode a dict to a JSON array .
+-}
+dict : (comparable -> Value) -> (v -> Value) -> Dict comparable v -> Value
+dict encodeKey encodeValue =
+  list << List.map (tuple2 encodeKey encodeValue) << Dict.toList
