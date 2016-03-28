96b52a8853e401120e0b0343fd45ea1d3719bd6f
Kris Jenkins <krisajenkins@gmail.com>
2016-03-28 17:39:29 +0100

    Adding some tuple-manipulation functions.

--- src/Exts/Tuple.elm
+++ src/Exts/Tuple.elm
@@ -1,8 +1,8 @@
-module Exts.Tuple (..) where
+module Exts.Tuple (indexedPair, first, second, (***), (&&&)) where
 
 {-| Extensions for tuples.
 
-@docs indexedPair
+@docs indexedPair, first, second, (***), (&&&)
 
 -}
 
@@ -14,3 +14,31 @@ module Exts.Tuple (..) where
 indexedPair : (a -> b) -> a -> ( b, a )
 indexedPair f x =
   ( f x, x )
+
+
+{-| Update the first component of a pair.
+-}
+first : (a -> a') -> ( a, b ) -> ( a', b )
+first f ( x, y ) =
+  ( f x, y )
+
+
+{-| Update the second component of a pair.
+-}
+second : (b -> b') -> ( a, b ) -> ( a, b' )
+second f ( x, y ) =
+  ( x, f y )
+
+
+{-| Update the both components of a pair.
+-}
+(***) : (a -> a') -> (b -> b') -> ( a, b ) -> ( a', b' )
+(***) fX fY ( x, y ) =
+  ( fX x, fY y )
+
+
+{-| Generate a pair from a single value and a left & right function.
+-}
+(&&&) : (a -> b) -> (a -> c) -> a -> ( b, c )
+(&&&) fX fY v =
+  ( fX v, fY v )
