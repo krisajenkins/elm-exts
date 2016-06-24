853a8a727f1d4c9e44cabc4765761a7e5bcd7344
Kris Jenkins <krisajenkins@gmail.com>
2016-06-24 16:24:41 +0100

    Refactoring the Date tests.

--- test/Tests/Exts/Date.elm
+++ test/Tests/Exts/Date.elm
@@ -1,24 +1,28 @@
 module Tests.Exts.Date exposing (tests)
 
+import Date exposing (..)
 import ElmTest exposing (..)
 import Exts.Date exposing (..)
-import Date exposing (..)
 
 
 tests : Test
 tests =
-    suite "Exts.Date" [ toISOStringTests ]
+    suite "Exts.Date"
+        [ toISOStringTests
+        ]
 
 
 toISOStringTests : Test
 toISOStringTests =
-    ElmTest.suite "toIsoString"
-        [ defaultTest
-            (assertEqual (Ok "2015-10-20T16:01:01.000Z")
-                (Result.map toISOString (Date.fromString "Tue Oct 20 2015 17:01:01"))
-            )
-        , defaultTest
-            (assertEqual (Ok "2015-10-21T00:00:00.000Z")
-                (Result.map toISOString (Date.fromString "Wed Oct 21 2015 01:00:00"))
-            )
-        ]
+    let
+        check ( from, to ) =
+            defaultTest
+                <| assertEqual (Ok to)
+                <| Result.map toISOString
+                <| Date.fromString from
+    in
+        ElmTest.suite "toIsoString"
+            <| List.map check
+                [ ( "Tue Oct 20 2015 17:01:01", "2015-10-20T16:01:01.000Z" )
+                , ( "Wed Oct 21 2015 01:00:00", "2015-10-21T00:00:00.000Z" )
+                ]
