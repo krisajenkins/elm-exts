84b52fe3a9aaa5f32baf53a91cb28bcf73e596db
Kris Jenkins <krisajenkins@gmail.com>
2016-12-16 10:16:00 +0000

    Adding Exts.Http.formBody.

--- src/Exts/Http.elm
+++ src/Exts/Http.elm
@@ -1,8 +1,13 @@
-module Exts.Http exposing (cgiParameters)
+module Exts.Http
+    exposing
+        ( cgiParameters
+        , formBody
+        )
 
 {-| Extensions to the `Http` library.
 
 @docs cgiParameters
+@docs formBody
 -}
 
 import Http exposing (..)
@@ -22,3 +27,12 @@ cgiParameters : List ( String, String ) -> String
 cgiParameters =
     List.map cgiParameter
         >> String.join "&"
+
+
+{-| Put some key-value pairs in the body of your `Request`. This will automatically
+add the `Content-Type: application/x-www-form-urlencoded` header.
+-}
+formBody : List ( String, String ) -> Body
+formBody =
+    cgiParameters
+        >> stringBody "application/x-www-form-urlencoded"
