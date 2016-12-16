c360d8f8e6b0765933ffca03785d7a607a7a373e
Kris Jenkins <krisajenkins@gmail.com>
2016-12-16 10:06:57 +0000

    Restoring Exts.Http.cgiParameters.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,6 @@ Big breaking changes for Elm 0.18.
 * Removed `RemoteData`. This now lives in [its own package][remotedata].
 * `Exts.Tuple.first` and `second` become `mapFirst` and `mapSecond`.
 [remotedata]: package.elm-lang.org/packages/krisajenkins/remotedata/latest
+* Removed most of `Exts.Http`. The new version of `elm-lang/http` is
+  much more user-friendly, so few of those supporting functions still
+  apply.
--- src/Exts/Http.elm
+++ src/Exts/Http.elm
@@ -1,96 +1,19 @@
-module Exts.Http exposing (put, postContent, postForm, postJson, handleError, cgiParameters)
+module Exts.Http exposing (cgiParameters)
 
 {-| Extensions to the `Http` library.
 
-@docs handleError
-@docs put
-@docs postContent
-@docs postForm
-@docs postJson
 @docs cgiParameters
 -}
 
 import Http exposing (..)
-import Json.Decode exposing (Decoder)
 import String
-import Task exposing (Task, andThen, mapError, succeed, fail)
-
-
-promoteError : RawError -> Error
-promoteError rawError =
-    case rawError of
-        RawTimeout ->
-            Timeout
-
-        RawNetworkError ->
-            NetworkError
-
-
-checkStatus : Response -> Task Error Response
-checkStatus response =
-    if 200 <= response.status && response.status < 300 then
-        Task.succeed response
-    else
-        Task.fail (BadResponse response.status response.statusText)
-
-
-{-| Lift a raw Http response into a `Task Error Response`, using the same rules `Http` uses internally.
--}
-handleError : Task RawError Response -> Task Error Response
-handleError t =
-    andThen (Task.mapError promoteError t)
-        checkStatus
-
-
-{-| Send a simple `PUT` request.
--}
-put : Decoder value -> String -> Http.Body -> Task Error value
-put decoder url body =
-    let
-        request =
-            { verb = "PUT"
-            , headers = []
-            , url = url
-            , body = body
-            }
-    in
-        fromJson decoder (send Http.defaultSettings request)
-
-
-{-| Send a `POST` request with the given content-type.
--}
-postContent : String -> Decoder value -> String -> Body -> Task Error value
-postContent contentType decoder url body =
-    let
-        request =
-            { verb = "POST"
-            , headers = [ ( "Content-Type", contentType ) ]
-            , url = url
-            , body = body
-            }
-    in
-        fromJson decoder (Http.send Http.defaultSettings request)
-
-
-{-| Send a `POST` request with appropriate headers for form payloads.
--}
-postForm : Decoder value -> String -> Body -> Task Error value
-postForm =
-    postContent "application/x-www-form-urlencoded"
-
-
-{-| Send a `POST` request with appropriate headers JSON payloads.
--}
-postJson : Decoder value -> String -> Body -> Task Error value
-postJson =
-    postContent "application/json"
 
 
 {-| Encode a CGI parameter pair.
 -}
 cgiParameter : ( String, String ) -> String
 cgiParameter ( key, value ) =
-    uriEncode key ++ "=" ++ uriEncode value
+    encodeUri key ++ "=" ++ encodeUri value
 
 
 {-| Encode a CGI parameter list.
